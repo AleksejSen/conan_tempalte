7e2335a9f3642b5d33649fad4cc766677ee60242
Abhay Raj Singh <abhayonlyone@gmail.com>
2025-01-21 02:12:45 +0530

    feat: success compiled

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
-#include <vector>
+#include "CLI/CLI.hpp"
 #include <fmt/ranges.h>
+#include <vector>
 
-int main() {
+int main(int argc, char **argv) {
+  CLI::App app{"App description"};
+
+  // Define options
+  int p = 0;
+  app.add_option("-p", p, "Parameter");
+
+  CLI11_PARSE(app, argc, argv);
+  fmt::print("Parameter value: {}\n", p);
+
   std::vector<int> v = {1, 2, 3};
   fmt::print("{}\n", v);
+
+  return 0;
 }
